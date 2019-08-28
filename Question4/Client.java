b977583344b7d0241a2d662cc8d5acd67fdc65c1
yh5598 <yh5598@utexas.edu>
2019-08-28 15:55:08 -0500

    working client-server communication

--- Question4/Client.java
+++ Question4/Client.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Client {
   public static void main (String[] args) {
 	  
+	
 	//Server.main(new String[] {25,67,'hi'});
 	  
     String hostAddress;
@@ -26,7 +27,7 @@ public class Client {
     udpPort = Integer.parseInt(args[2]);
     
     //UDP buffer packet
-    byte[] udprbuffer = new byte[1024];
+    byte[] udprbuffer = new byte[50];
     
     Scanner sc = new Scanner(System.in);
     while(sc.hasNextLine()) {
@@ -52,7 +53,7 @@ public class Client {
     		  try {
     			InetAddress ia = InetAddress.getByName(hostAddress);
 				DatagramSocket datasocket = new DatagramSocket();
-				String message = "user1 "+productName+" "+quantity;
+				String message = "purchase user1 "+productName+" "+quantity;
 				byte[] buffer = new byte[message.length()];
 				buffer=message.getBytes();
 				DatagramPacket sPacket, rPacket;
@@ -84,12 +85,114 @@ public class Client {
       } else if (tokens[0].equals("cancel")) {
         // TODO: send appropriate command to the server and display the
         // appropriate responses form the server
+    	  String cancelOrderNumber = tokens[1];
+    	  if(mode=="UDP"){
+    		  try {
+    			InetAddress ia = InetAddress.getByName(hostAddress);
+				DatagramSocket datasocket = new DatagramSocket();
+				String message = "cancel "+cancelOrderNumber;
+				byte[] buffer = new byte[message.length()];
+				buffer=message.getBytes();
+				DatagramPacket sPacket, rPacket;
+				sPacket = new DatagramPacket(buffer, buffer.length, ia, udpPort);
+				System.out.println("got buffer");
+				datasocket.send(sPacket);
+				
+				rPacket = new DatagramPacket(udprbuffer, udprbuffer.length);
+				System.out.println("getting response");
+				datasocket.receive(rPacket);
+				System.out.println("got response");
+				String recieveString = new String(rPacket.getData(), 0, rPacket.getLength());
+				System.out.println("Cancel status: "+recieveString);
+				
+			} catch (SocketException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+    		  
+    	  }
+    	  else{
+    		  
+    	  }
+    	  
       } else if (tokens[0].equals("search")) {
         // TODO: send appropriate command to the server and display the
         // appropriate responses form the server
+    	  String username = tokens[1];
+    	  if(mode=="UDP"){
+    		  try {
+    			InetAddress ia = InetAddress.getByName(hostAddress);
+				DatagramSocket datasocket = new DatagramSocket();
+				String message = "search "+username;
+				byte[] buffer = new byte[message.length()];
+				buffer=message.getBytes();
+				DatagramPacket sPacket, rPacket;
+				sPacket = new DatagramPacket(buffer, buffer.length, ia, udpPort);
+				System.out.println("got buffer");
+				datasocket.send(sPacket);
+				
+				rPacket = new DatagramPacket(udprbuffer, udprbuffer.length);
+				System.out.println("getting response");
+				datasocket.receive(rPacket);
+				System.out.println("got response");
+				String recieveString = new String(rPacket.getData(), 0, rPacket.getLength());
+				System.out.println("User order history: "+recieveString);
+				
+			} catch (SocketException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+    		  
+    	  }
+    	  else{
+    		  
+    	  }
       } else if (tokens[0].equals("list")) {
         // TODO: send appropriate command to the server and display the
         // appropriate responses form the server
+    	  if(mode=="UDP"){
+    		  try {
+    			InetAddress ia = InetAddress.getByName(hostAddress);
+				DatagramSocket datasocket = new DatagramSocket();
+				String message = "list";
+				byte[] buffer = new byte[message.length()];
+				buffer=message.getBytes();
+				DatagramPacket sPacket, rPacket;
+				sPacket = new DatagramPacket(buffer, buffer.length, ia, udpPort);
+				System.out.println("got buffer");
+				datasocket.send(sPacket);
+				
+				rPacket = new DatagramPacket(udprbuffer, udprbuffer.length);
+				System.out.println("getting response");
+				datasocket.receive(rPacket);
+				System.out.println("got response");
+				String recieveString = new String(rPacket.getData(), 0, rPacket.getLength());
+				System.out.println("Available products: \n"+recieveString);
+				
+			} catch (SocketException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+    		  
+    	  }
+    	  else{
+    		  
+    	  }
       } else {
         System.out.println("ERROR: No such command");
       }
--- Question4/Server.java
+++ Question4/Server.java
@@ -5,52 +5,125 @@ import java.net.*;
 import java.util.ArrayList;
 
 public class Server {
+	
+	
+	static ArrayList<String> purchaseOrder = new ArrayList<String>();
+	
 	  public static void main (String[] args) throws IOException {
-	    int tcpPort;
-	    int udpPort;
-	    if (args.length != 3) {
-	      System.out.println("ERROR: Provide 3 arguments");
-	      System.out.println("\t(1) <tcpPort>: the port number for TCP connection");
-	      System.out.println("\t(2) <udpPort>: the port number for UDP connection");
-	      System.out.println("\t(3) <file>: the file of inventory");
-
-	      System.exit(-1);
-	    }
-	    tcpPort = Integer.parseInt(args[0]);
-	    udpPort = Integer.parseInt(args[1]);
-	    String fileName = args[2];
-	    //System.out.println(System.getProperty("user.dir"));
-	    // parse the inventory file
-	    File file = new File(System.getProperty("user.dir")+"\\src\\input\\inventory.txt"); 
-	    ArrayList<String> inventory = new ArrayList<String>();
-	    BufferedReader br = new BufferedReader(new FileReader(file)); 
-	    
-	    String st; 
-	    while ((st = br.readLine()) != null) 
-	    	inventory.add(st);
-	   
-	    // TODO: handle request from clients
-	  	byte[] buf = new byte[1024];
-	  	DatagramPacket datapacket, returnpacket;
-	  	try{
-		  	DatagramSocket datasocket= new DatagramSocket(udpPort);
-		  	
-		  	while(true){
-		  		datapacket=new DatagramPacket(buf, buf.length);
-		  		datasocket.receive(datapacket);
-		  		returnpacket = new DatagramPacket(datapacket.getData(),
-		  										datapacket.getLength(),
-		  										datapacket.getAddress(),
-		  										datapacket.getPort());
-		  		datasocket.send(returnpacket);
+		    int tcpPort;
+		    int udpPort;
+		    
+		    //server orderID for all customers
+		    int orderID=1;
+		    if (args.length != 3) {
+		      System.out.println("ERROR: Provide 3 arguments");
+		      System.out.println("\t(1) <tcpPort>: the port number for TCP connection");
+		      System.out.println("\t(2) <udpPort>: the port number for UDP connection");
+		      System.out.println("\t(3) <file>: the file of inventory");
+	
+		      System.exit(-1);
+		    }
+		    tcpPort = Integer.parseInt(args[0]);
+		    udpPort = Integer.parseInt(args[1]);
+		    String fileName = args[2];
+		    //System.out.println(System.getProperty("user.dir"));
+		    // parse the inventory file
+		    File file = new File(System.getProperty("user.dir")+"\\src\\input\\inventory.txt"); 
+		    ArrayList<String> inventory = new ArrayList<String>();
+		    
+		    BufferedReader br = new BufferedReader(new FileReader(file)); 
+		    
+		    String st; 
+		    while ((st = br.readLine()) != null) 
+		    	inventory.add(st);
+		   
+		    // TODO: handle request from clients
+		  	byte[] buf = new byte[65535];
+		  	DatagramPacket datapacket, returnpacket;
+		  	try{
+			  	DatagramSocket datasocket= new DatagramSocket(udpPort);
+			  	
+			  	while(true){
+			  		datapacket=new DatagramPacket(buf, buf.length);
+			  		datasocket.receive(datapacket);
+			  		String data = new String(datapacket.getData());
+			  		
+			  		
+			  		String command = data.substring(0, data.indexOf(" "));
+			  		System.out.println(command);
+			  		byte [] returnByte=null;
+			  		switch(command) {
+			  		case "purchase":
+			  			returnByte = UDPPurchase(orderID, data, inventory);
+			  			System.out.println(new String(returnByte));
+			  			break;
+			  		case "cancel":
+			  			returnByte = UDPCancel(orderID);
+			  			break;
+			  		case "search":
+			  			returnByte = UDPSearch();
+			  			break;
+			  		case "list":
+			  			returnByte = UDPList();
+			  			break;
+			  		}
+			  		
+			  		returnpacket = new DatagramPacket(returnByte,
+			  										datapacket.getLength()+100,
+			  										datapacket.getAddress(),
+			  										datapacket.getPort());
+			  		datasocket.send(returnpacket);
+			  	}
+		  	}catch(SocketException e){
+		  		
+		  		
 		  	}
-	  	}catch(SocketException e){
-	  		
-	  		
-	  	}
-	  	
-	  	
 	  	
 	  
-	  } 
+	  }
+
+	private static byte [] UDPList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static byte [] UDPSearch() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static byte [] UDPCancel(int orderID) {
+		// TODO Auto-generated method stub
+		String id = String.valueOf(orderID);
+		for ()
+		
+		return null;
+	}
+
+	private static byte [] UDPPurchase(int orderID, String data, ArrayList<String> inventory) {
+		// TODO Auto-generated method stub
+		
+		//return responses
+		String returnedStringPurchaseComplete = "Your order has been placed, OrderID"+String.valueOf(orderID)+" "+data.substring(data.indexOf(" ")+1);
+		String [] returnResponses = {"Not Available - We do not sell this product","Not Available - Not enough items",returnedStringPurchaseComplete};
+		
+		
+		String [] commandList = data.split("\\s+");
+		
+		//check if item is in inventory, not enough item, or enough item
+		for (int x=0; x<inventory.size() ;x++) {
+			if (commandList[2].contentEquals(inventory.get(x).substring(0, inventory.get(x).indexOf(" ")))) {
+				//
+				if(Integer.valueOf(commandList[3].replaceAll("\\D+", ""))>Integer.valueOf(inventory.get(x).replaceAll("\\D+", ""))) {
+					return returnResponses[1].getBytes();
+				}
+				else {
+					purchaseOrder.add(String.valueOf(orderID)+" "+data.substring(data.indexOf(" ")+1));
+					return returnResponses[2].getBytes();
+				}
+			}
+		}
+		
+		return returnResponses[0].getBytes();
+	} 
 }
