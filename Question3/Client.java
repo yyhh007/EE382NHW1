c05685d60932131dd2fbe65d6586e93b1af92356
yh5598 <yh5598@utexas.edu>
2019-10-08 22:57:57 -0500

    timeout client

--- Question3/Client.java
+++ Question3/Client.java
@@ -2,7 +2,11 @@ package Question3;
 
 import java.io.IOException;
 import java.io.PrintStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Scanner;
 
 
@@ -14,19 +18,70 @@ public class Client {
 	int pid=1;
 	LamportClock c;
 	//open tcp socket
-	public void getSocket(String hostAddress, int port) throws IOException{
+	public void getSocket(String hostAddress, int port, String [] serverAddressArray, int [] portlist) throws IOException{
 		
-		server = new Socket(hostAddress, port);
+		//server = new Socket(hostAddress, port);
+		Instant first = Instant.now();
+		Instant second = Instant.now();
+
+		Duration duration =  Duration.between(first,second);
+		server = new Socket();
+		//if IO exception then loop for 
+		long numMillis = 0;
+		//loops on  each server in the list
+		//if the server fails over 100ms
+			//next server
+		//else
+			//break
+		for(int i =0; i<serverAddressArray.length;i++) {
+			
+			numMillis = 0;
+			first = Instant.now();		
+			while(numMillis <101) {
+				try {
+				if(server.isConnected()) {
+						break;
+				}
+				second = Instant.now();
+				duration =  Duration.between(first,second);
+				numMillis = duration.toMillis();
+				//if the connection passed then break
+				
+				server.connect(new InetSocketAddress(serverAddressArray[i],portlist[i]), 100);
+				
+				}catch (SocketTimeoutException e) {
+					
+					e.printStackTrace();
+					break;
+				}  
+				catch (IOException e) {
+					e.printStackTrace();}
+			}
+			
+			//if the server fails over 100ms
+			if(numMillis<100) {
+				break;
+			} 
+			
+			if (i==serverAddressArray.length-1) {
+				i = 0;
+			}
+				
+				
+		}
+
+	
 		din = new Scanner(server.getInputStream());
 		pout = new PrintStream(server.getOutputStream());
+		
 	}
 	
 	//TCP send receive print
-	
+	//
 	//add timeout here 100mill
 	//retry on timeout here, loop through list of server ips
-	public String TCPSendClientRequest(String hostAddress, int tcpPort, String outMessage) throws IOException {
-		 getSocket(hostAddress, tcpPort);
+	public String TCPSendClientRequest(String hostAddress, int tcpPort, String [] serverAddressArray, int [] portlist, String outMessage) throws IOException {
+		 getSocket(hostAddress, tcpPort,serverAddressArray,portlist);
 		 pout.println(outMessage);
 		 pout.flush();
 		 String returnString = din.nextLine();
@@ -46,8 +101,13 @@ public class Client {
 		int tcpPortIndex = 0;
 		//run arguments
 		//getting all the ports 
+		
 		numberOfServer = Integer.parseInt(args[0]);
 		hostAddress = args[1].substring(0, args[1].indexOf(":"));
+		String [] serverAddressArray =new String[numberOfServer];
+		serverAddressArray[0] = "127.0.0.1";
+		serverAddressArray[1] = "127.0.0.1";
+		
 		for (int i = 1; i<args.length; i++) 
 			//System.out.println(args[i].substring(args[i].indexOf(":")+1));
 			tcpPorts[i-1] = Integer.parseInt(args[i].substring(args[i].indexOf(":")+1));
@@ -55,7 +115,7 @@ public class Client {
 		//System.out.println(tcpPorts);
 	    Scanner sc = new Scanner(System.in);
 	    /*int numServer = sc.nextInt();
-	
+		
 	    for (int i = 0; i < numServer; i++) {
 	      // TODO: parse inputs to get the ips and ports of servers
 	    }*/
@@ -84,9 +144,11 @@ public class Client {
 	        System.out.println("ERROR: No such command");
 	    	}
 	      	message+=" 1";
+	      	
+	      	
 	      	try {
 	      		System.out.println("resquesting message: "+message+" at port "+hostAddress+Integer.toString(tcpPorts[tcpPortIndex]));
-				String response = client.TCPSendClientRequest(hostAddress, tcpPorts[tcpPortIndex], message);
+				String response = client.TCPSendClientRequest(hostAddress, tcpPorts[tcpPortIndex],serverAddressArray,tcpPorts , message);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
