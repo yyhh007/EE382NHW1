d1f6abfd461f87caf66b56c0ff3ef70946915f92
yh5598 <yh5598@utexas.edu>
2019-10-10 10:44:42 -0500

    server input method change

--- Question3/Server.java
+++ Question3/Server.java
@@ -22,101 +22,40 @@ if server
 public class Server {
 	public static void main (String[] args) throws IOException{
 
-		//Scanner sc = new Scanner(System.in);
-		//int myID = sc.nextInt();
-		//int numServer = sc.nextInt();
-		//int numSeat = sc.nextInt();
+		
+		Scanner sc = new Scanner(System.in);
+		int myID = sc.nextInt();
+		int numServer = sc.nextInt();
+		int numSeat = sc.nextInt();
 		
 		
-		new Thread(new Runnable() {
-	    	public void run() {
-	    		System.out.println("tcp server on port 8025 started:");	
-	    		try {
-	    			ServerSocket listener  = new ServerSocket(8025);
-	    			Socket s;
-	    			LamportClock c = new LamportClock();
-	    			Seats seat = new Seats(10);
-	    			Queue <Timestamp>requestq = requestq = new PriorityQueue<Timestamp>(5, new Comparator<Timestamp>() {
-	    				public int compare(Timestamp a, Timestamp b) {return Timestamp.compare(a, b);}	
-	    			});
-	    			while ((s = listener.accept())!= null) {
-	    				Thread t = new TCPServerThread(10, c, s, requestq, seat, 2, new int [] {8030, 8035}, false);
-	    				//((TCPServerThread) t).initSeats();
-	    				t.start();
-	    			}
-	    		}catch(IOException e) {
-	    			System.out.println(e);
-	    		}
-	    	}
-	    }).start();
-		
-		new Thread(new Runnable() {
-	    	public void run() {
-	    		System.out.println("tcp server on port 8030 started:");	
-	    		try {
-	    			ServerSocket listener  = new ServerSocket(8030);
-	    			Socket s;
-	    			Seats seat = new Seats(10);
-	    			LamportClock c = new LamportClock();
-	    			Queue <Timestamp>requestq = requestq = new PriorityQueue<Timestamp>(5, new Comparator<Timestamp>() {
-	    				public int compare(Timestamp a, Timestamp b) {return Timestamp.compare(a, b);}	
-	    			});
-	    			while ((s = listener.accept())!= null) {
-	    				Thread t = new TCPServerThread(10, c, s, requestq, seat, 2, new int [] {8025, 8035}, false);
-	    				t.start();
-	    			}
-	    		}catch(IOException e) {
-	    			System.out.println(e);
-	    		}
-	    	}
-	    }).start();
-		
-		new Thread(new Runnable() {
-	    	public void run() {
-	    		System.out.println("tcp server on port 8035 started:");	
-	    		try {
-	    			ServerSocket listener  = new ServerSocket(8035);
-	    			Socket s;
-	    			Seats seat = new Seats(10);
-	    			LamportClock c = new LamportClock();
-	    			Queue <Timestamp>requestq = requestq = new PriorityQueue<Timestamp>(5, new Comparator<Timestamp>() {
-	    				public int compare(Timestamp a, Timestamp b) {return Timestamp.compare(a, b);}	
-	    			});
-	    			while ((s = listener.accept())!= null) {
-	    				Thread t = new TCPServerThread(10,c, s, requestq, seat, 2, new int [] {8025, 8030}, true);
-	    				t.start();
-	    			}
-	    		}catch(IOException e) {
-	    			System.out.println(e);
-	    		}
-	    	}
-	    }).start();
-		
-		/*for (int i = 0; i < numServer; i++) {
-		  // TODO: parse inputs to get the ips and ports of servers
-			String serverIP = sc.nextLine();
-			
-			
-			
+		for (int i = 0; i < numServer; i++) {
+		  	// TODO: parse inputs to get the ips and ports of servers
+			int portNumber = sc.nextInt();
 			
 			new Thread(new Runnable() {
 		    	public void run() {
-		    		System.out.println("tcp server on port 8030 started:");	
+		    		System.out.println("tcp server on port 8025 started:");	
 		    		try {
-		    			ServerSocket listener  = new ServerSocket(8030);
+		    			ServerSocket listener  = new ServerSocket(portNumber);
 		    			Socket s;
-		    			
+		    			LamportClock c = new LamportClock();
+		    			Seats seat = new Seats(numSeat);
+		    			Queue <Timestamp>requestq = requestq = new PriorityQueue<Timestamp>(numServer, new Comparator<Timestamp>() {
+		    				public int compare(Timestamp a, Timestamp b) {return Timestamp.compare(a, b);}	
+		    			});
 		    			while ((s = listener.accept())!= null) {
-		    				//Thread t = new TCPServerThread(s);
-		    				//t.start();
+		    				Thread t = new TCPServerThread(numSeat, myID+i, c, s, requestq, seat, 2, new int [] {8030, 8035}, false);
+		    				//((TCPServerThread) t).initSeats();
+		    				t.start();
 		    			}
 		    		}catch(IOException e) {
 		    			System.out.println(e);
 		    		}
 		    	}
 		    }).start();
-		}*/
-		
-		// TODO: handle request from clients
+			
+			
+		}		
 	}
 }
